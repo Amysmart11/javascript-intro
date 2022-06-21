05a740f451a896b8496c406a2343279c02863b47
AMYSMART <amabouchiamaka130@gmail.com>
2022-06-21 03:10:36 -0700

    introduction to javascript

---
+++ script.js
@@ -0,0 +1,5 @@
+var n = prompt('enter your name');
+var height = prompt('enter your height');
+var country = prompt('enter your country');
+
+document.write("Hi " + n +"." + " Your height is " + height+"m" +"." + " You are from "  + country);
